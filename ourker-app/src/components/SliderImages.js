4333daff115ef4df24aec42886d35416322661e9
FernandoDGU <66134044+FernandoDGU@users.noreply.github.com>
2022-03-30 00:08:34 -0600

    Component SliderImage

---
+++ ourker-app/src/components/SliderImages.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Slider from 'react-slick'
+
+const images = [
+        {
+          photo:
+          "busqueda_filtros.jpg",
+          label: "image1"
+        },
+        {
+            photo:
+            "cover.png",
+            label: "image1"
+          },
+        {
+          photo:
+          'fondo1.jpg',
+          label: "image2"
+        }
+      ];
+
+
+export default function SliderImages() {
+  var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slickNext: true,
+        slickPrevious: true,
+        swipe: true
+      };
+  
+    return (
+     <Slider {...settings}>
+                {images.map((step) => (
+                <div key={step.label}>
+                    <img
+                    src={step.photo}
+                    alt={step.label}
+                    style={{
+                        // borderRadius: "15px",
+                        height: "380px",
+                        display: "block",
+                        overflow: "hidden",
+                        width: "100%"
+                    }}
+                    />
+                </div>
+                ))}
+            </Slider>      
+  )
+}
--- ourker-app/src/pages/Report.js
+++ ourker-app/src/pages/Report.js
@@ -1,40 +1,10 @@
 import React from 'react'
 import { Button, CardMedia, Grid, Paper, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import Slider from 'react-slick'
-
-const images = [
-    {
-      photo:
-      "busqueda_filtros.jpg",
-      label: "image1"
-    },
-    {
-        photo:
-        "cover.png",
-        label: "image1"
-      },
-    {
-      photo:
-      'fondo1.jpg',
-      label: "image2"
-    }
-  ];
+import SliderImages from '../components/SliderImages'
 
 
 export default function Report() {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        slickNext: true,
-        slickPrevious: true,
-        swipe: true
-      };
 
   return (
     <Grid spacing={2} style={{
@@ -82,23 +52,7 @@ export default function Report() {
 
             <Box height={'20px'}/>
             <Box padding={3}>
-            <Slider {...settings}>
-                {images.map((step) => (
-                <div key={step.label}>
-                    <img
-                    src={step.photo}
-                    alt={step.label}
-                    style={{
-                        // borderRadius: "15px",
-                        height: "380px",
-                        display: "block",
-                        overflow: "hidden",
-                        width: "100%"
-                    }}
-                    />
-                </div>
-                ))}
-            </Slider>    
+            <SliderImages/>
             </Box>
             
             
