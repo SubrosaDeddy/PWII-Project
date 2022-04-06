813638161093bf811356cfc3e43d91800443acdc
Daniel Luna <danlf9919@gmail.com>
2022-04-05 23:10:29 -0500

    Eliminada UserChat

--- ourker-app/src/App.js
+++ ourker-app/src/App.js
@@ -5,7 +5,6 @@ import { Helmet } from 'react-helmet';
 import Home from './pages/Home';
 import LogIn from './pages/LogIn';
 import SignIn from './pages/SignIn';
-import UserChat from './pages/UserChat';
 import NavBar from './components/Navbar'
 import ChatWorker from './pages/ChatWorker';
 import CreateReport from './pages/CreateReport';
@@ -22,14 +21,12 @@ function App() {
       <NavBar/>
       <Helmet> <title>Ourker</title> </Helmet>
       <Routes>
-      <Route exact path="/" element={<Home/>} />
-      <Route exact path="SignIn" element={<SignIn/>} />
-      <Route exact path="LogIn" element={<LogIn/>} />
-      <Route exact path="CreateReport" element={<CreateReport/>} />
-      <Route exact path="CreatePost" element={<CreatePost/>} />
-      <Route exact path="Prueba" element={<UserChat/>} />
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="SignIn" element={<SignIn/>} />
+        <Route exact path="LogIn" element={<LogIn/>} />
+        <Route exact path="CreateReport" element={<CreateReport/>} />
+        <Route exact path="CreatePost" element={<CreatePost/>} />
       </Routes>
-     
 
     </Fragment>
   );
