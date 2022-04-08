52528316eefbd6c70e45c33130c6dcde414ce128
Daniel Luna <danlf9919@gmail.com>
2022-04-08 02:06:03 -0500

    Overflow scroll en search

--- ourker-app/src/pages/Search.js
+++ ourker-app/src/pages/Search.js
@@ -14,18 +14,13 @@ export default function Search() {
     <Box sx={{backgroundImage: "linear-gradient(to top, #04448c,#00236f)", overflow:"hidden", height:"100vh", width:"100%", m:0, p:0}}>
       <Grid container sx={{backgroundColor:color_one.primary.secondary, width:"70%", m:"auto", marginTop:"10px", padding:"30px"}}>
         <Container sx={{borderWidth:"100px", borderColor:"black"}}>
-        <Grid sx={{backgroundColor:color_one.primary.main, m:0, py:3, px:1, width:1, alignItems:"center", justifyContent:"center"}}>
-          <SearchBar/>
-          </Grid>
+          <Grid sx={{backgroundColor:color_one.primary.main, m:0, py:3, px:1, width:1, alignItems:"center", justifyContent:"center"}}>
+            <SearchBar/>
+            </Grid>
         </Container>
-        <Container>
-
-
-        <Grid container sx={{px:"30px", py:"15px", my:"15px", borderRadius:"15px"}}>
-       
-
-        </Grid>
-
+        <Container sx={{height:"650px", overflowY:"scroll", marginTop:"25px"}}>
+          <Grid container sx={{px:"30px", py:"15px", my:"15px", borderRadius:"15px"}}>
+          </Grid>
           <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
             <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong> </Typography>
@@ -43,6 +38,25 @@ export default function Search() {
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
             <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
           </Grid>
+          
+          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+            <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
+            <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
+            <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
+          </Grid>
+          
+          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+            <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
+            <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
+            <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
+          </Grid>
+          
+          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+            <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
+            <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
+            <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
+          </Grid>
+
         </Container>
       </Grid>
     </Box>
