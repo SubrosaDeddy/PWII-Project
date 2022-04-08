4b277a99b4d42e63ca6cb4cbcef3d492f4a198b0
Edgar Adrian <edgar_olocoons@hotmail.com>
2022-04-08 03:05:04 -0500

    Search_Profile_Button
    OnClick en grids para ir a ventana de perfil de worker

--- ourker-app/src/pages/Search.js
+++ ourker-app/src/pages/Search.js
@@ -5,11 +5,25 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import {color_one} from "../utils/Themes";
 import SearchBar from '../components/SearchBar';
-import { Image } from 'mui-image';
+import { useNavigate } from 'react-router-dom';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import { styled } from '@mui/material/styles';
 
+const LightTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}));
 
 export default function Search() {
   
+  const navigate = useNavigate();
+
   return (
     <Box sx={{backgroundImage: "linear-gradient(to top, #04448c,#00236f)", overflow:"hidden", height:"100vh", width:"100%", m:0, p:0}}>
       <Grid container sx={{backgroundColor:color_one.primary.secondary, width:"70%", m:"auto", marginTop:"10px", padding:"30px"}}>
@@ -21,41 +35,54 @@ export default function Search() {
         <Container sx={{height:"650px", overflowY:"scroll", marginTop:"25px"}}>
           <Grid container sx={{px:"30px", py:"15px", my:"15px", borderRadius:"15px"}}>
           </Grid>
-          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+
+          <LightTooltip  title="Ir al perfil"  arrow>
+          <Grid container  onClick={() => navigate('/Perfil')} sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
             <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong> </Typography>
             <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
           </Grid>
+          </LightTooltip>
           
-          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+          <LightTooltip  title="Ir al perfil"  arrow>
+          <Grid container  onClick={() => navigate('/Perfil')} sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
             <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
             <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
           </Grid>
-          
-          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+          </LightTooltip>
+
+          <LightTooltip  title="Ir al perfil"  arrow>
+          <Grid container   onClick={() => navigate('/Perfil')} sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
             <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
             <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
           </Grid>
+          </LightTooltip>
           
-          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+          <LightTooltip  title="Ir al perfil"  arrow>
+          <Grid container  onClick={() => navigate('/Perfil')} sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
             <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
             <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
           </Grid>
-          
-          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+          </LightTooltip>
+
+          <LightTooltip  title="Ir al perfil"  arrow>
+          <Grid container  onClick={() => navigate('/Perfil')} sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
             <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
             <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
           </Grid>
+          </LightTooltip>
           
-          <Grid container sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
+          <LightTooltip  title="Ir al perfil"  arrow>
+          <Grid container  onClick={() => navigate('/Perfil')} sx={{px:"30px", py:"15px", my:"15px", backgroundColor:"#94C6FF", borderRadius:"15px"}}>
             <Avatar item xs={5} sx={{m:"5px", width:"30px", height:"30px"}}  src="/broken-image.jpg"></Avatar>
             <Typography item xs={7} sx={{marginLeft:"15px", my:"auto"}}> <strong> Paquito Perez Papitas Papas </strong></Typography>
             <Typography item sx={{marginTop:"10px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</Typography>
           </Grid>
+          </LightTooltip>
 
         </Container>
       </Grid>
