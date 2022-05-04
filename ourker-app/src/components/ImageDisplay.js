97f70f2a57b7850625344ed2c73ff20d4be72a9b
Daniel Luna <danlf9919@gmail.com>
2022-05-04 01:42:09 -0500

    Image Display y problemas con carga de imágenes

---
+++ ourker-app/src/components/ImageDisplay.js
@@ -0,0 +1,35 @@
+import React, {Fragment, useState} from 'react'
+import { Grid, CardMedia, Card, Typography, Paper } from '@mui/material'
+
+export default function ImageDisplay(props) {
+    let imgArray = [];
+    if(props.images.length)
+    {
+        props.images.forEach(e => {
+            imgArray.push(e);
+        });
+    }
+
+    if(imgArray.length)
+    {
+        return (
+        <Fragment>
+            <Grid container sx={{zIndex:"1", width:1, justifyContent:"space-around", width:"85%", mx:"auto", backgroundColor:"rgba(150,150,150,.7)", py:"25px", borderRadius:"15px"}}>
+                {imgArray.map((e) => (
+                    <Grid item xs={3} sx={{zIndex:"1"}}>
+                        <Card sx={{zIndex:"1"}}>
+                            <CardMedia sx={{height:300, width:"auto", zIndex:"1"}} component="img" image={e}/>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Fragment>
+      )
+    }
+    else
+    {
+        return(
+            <Fragment/>
+        )
+    }
+}
--- ourker-app/src/pages/CreatePost.js
+++ ourker-app/src/pages/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import Fab from '@mui/material/Fab';
 import {color_one} from "../utils/Themes";
 import { createTheme } from '@mui/material/styles';
@@ -8,6 +8,7 @@ import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import CardMedia from "@material-ui/core/CardMedia";
 import Post from "../models/Post";
 import { InsertPost, GetPost, GetAll } from '../services/PostService';
+import ImageDisplay from '../components/ImageDisplay';
 
 const theme = createTheme({
     typography: {
@@ -25,14 +26,26 @@ const theme = createTheme({
    
   });
 
-
-
 export default function CreatePost(props) 
 {
 
     const options = ["Mecánico", "Carpintero"];
     const [value, setValue] = React.useState();
     const [inputValue, setInputValue] = React.useState("");
+    
+    const [images, setImages] = React.useState([]);
+    const [imagesURL, setImagesURL] = React.useState([]);
+
+    useEffect(() => {
+      if (images) {
+        alert("Sí");
+        setImagesURL(window.URL.createObjectURL(images[0]));
+      }
+      else
+      {
+        alert(`Length: ${images.array.length}`);
+      }
+    }, [images]);
 
     const handleSubmit = async(event) =>{
       event.preventDefault();
@@ -67,34 +80,32 @@ export default function CreatePost(props)
 
   return ( 
     <Fragment >
-      <Paper  sx={{backgroundImage: 'url(/fondo3.jpg)', height: '67vh'}}>
+      <Paper  sx={{minHeight: "calc(100vh - 64px)", backgroundImage: 'url(/fondo3.jpg)'}}>
         <Grid spacing={1}>
             <Box height={'15px'}/>
             <Paper elevation={3} sx={{p: 2,margin: 'auto',maxWidth: 500,flexGrow: 1,top: 9,backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#1A2027' : '#fff'}}>
 
-              <Grid container spaicing= {1} xl={10} m={'auto'} sx={{ display:"flex", alignItems:"center"}}>
+              <Grid container spacing= {1} xl={10} m={'auto'} sx={{ display:"flex", alignItems:"center"}}>
                   
               <Box component="form" noValidate sx={{backgroundColor: color_one.primary.secondary}} onSubmit={handleSubmit}>
                 <Box height={'10px'}/>
-                {/* <form> */}
-                  <TextField margin="normal" id="title" label="Titulo" name="title" variant="outlined" required  fullWidth/> 
-                  <TextField margin="normal"  id="description" label="Descripción" name="description" variant="outlined" required multiline fullWidth maxRows={5}/> 
-                  <Autocomplete value={value} 
-                  // onChange={(event, newValue) => {setValue(newValue)}} 
-                  // inputValue={inputValue} 
-                    options={options} sx={{mx:"auto"}}
-                    onInputChange={(event, newInputValue) => {
-                      setInputValue(newInputValue);
-                    }}
-                    renderInput={(params) => (
-                      <TextField {...params} label="Categoria" id="category" name="category" required/>
-                  )}/>
-                  <Box m={2} pt={4}>
-                  <Button fullWidth="true" sx={{mx:"auto"}} type='submit' variant="contained" startIcon={<SendIcon />}>
-                  Publicar 
-                  </Button>
-                  </Box>
-              {/* </form> */}
+                    <TextField margin="normal" id="title" label="Titulo" name="title" variant="outlined" required  fullWidth/> 
+                    <TextField margin="normal"  id="description" label="Descripción" name="description" variant="outlined" required multiline fullWidth maxRows={5}/> 
+                    <Autocomplete value={value} 
+                    // onChange={(event, newValue) => {setValue(newValue)}} 
+                    // inputValue={inputValue} 
+                      options={options} sx={{mx:"auto"}}
+                      onInputChange={(event, newInputValue) => {
+                        setInputValue(newInputValue);
+                      }}
+                      renderInput={(params) => (
+                        <TextField {...params} label="Categoria" id="category" name="category" required/>
+                    )}/>
+                    <Box m={2} pt={4}>
+                    <Button fullWidth="true" sx={{mx:"auto"}} type='submit' variant="contained" startIcon={<SendIcon />}>
+                    Publicar 
+                    </Button>
+                    </Box>
               </Box>
 
               </Grid>
@@ -106,7 +117,7 @@ export default function CreatePost(props)
         <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '15vh' }}>
           <Grid item xs={3}>
             <label htmlFor="upload-photo">
-                <input style={{ display: 'none' }} id="upload-photo" name="upload-photo" type="file" accept="image/*"/>
+                <input style={{ display: 'none' }} id="upload-photo" name="upload-photo" type="file" accept="image/*" onChange={(e) => setImages(e.target.files[0])}/>
                 <br/>
                 <Fab color="primary" size="big" component="span" aria-label="add">
                   <AddPhotoAlternateIcon />
@@ -114,33 +125,9 @@ export default function CreatePost(props)
             </label>
           </Grid>
         </Grid>
+        {/* <ImageDisplay images={["https://i.pinimg.com/736x/f7/b2/7a/f7b27a3ac6a79a9414ac208931e0da3c--beluga-whales.jpg", "https://i.pinimg.com/736x/f7/b2/7a/f7b27a3ac6a79a9414ac208931e0da3c--beluga-whales.jpg", "https://i.pinimg.com/736x/f7/b2/7a/f7b27a3ac6a79a9414ac208931e0da3c--beluga-whales.jpg"]}/> */}
+        <ImageDisplay images={imagesURL}/>
       </Paper>
-
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: '50vh' }}
-      >
-
-        <Grid item xs={3}>
-
-        <Card>
-      
-      <CardMedia>
-      <img src="/busqueda_workers.jpg"  width={500} alt="recipe thumbnail"/>
-      <img src="/busqueda_filtros.jpg"  width={500} alt="recipe thumbnail"/>
-      <img src="/work_figures.jpg"  width={500} alt="recipe thumbnail"/>
-      </CardMedia>
-
-      </Card>
-
-        </Grid>   
-        
-      </Grid> 
-
     </Fragment>
   )
 }
