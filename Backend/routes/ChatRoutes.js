5dfdc44952a4e6eade5b28f1095399728090f3ce
Aivanr27 <aivanr27@gmail.com>
2022-05-08 02:32:47 -0500

    ChatController
    getChats 1 a 1

--- Backend/routes/ChatRoutes.js
+++ Backend/routes/ChatRoutes.js
@@ -7,5 +7,6 @@ router.get("/chat", chat_controller.chat_getall);
 router.post("/chat", chat_controller.chat_create);
 router.get("/chat/:id", chat_controller.chat_getallChats);
 router.delete("/chat/:id", chat_controller.chat_delete);
+router.get("/chat/:id1/:id2",chat_controller.chat_messagesget);
 
 module.exports = router;
