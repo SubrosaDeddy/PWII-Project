24e64e977edc593fdb237fa65ae302572b18e1b5
FernandoDGU <66134044+FernandoDGU@users.noreply.github.com>
2022-05-13 02:19:00 -0500

    Controlador Localidades Ocupaciones

--- Backend/routes/WorkerRoutes.js
+++ Backend/routes/WorkerRoutes.js
@@ -16,6 +16,8 @@ router.get("/worker/ocupation/:id", worker_controller.worker_ocupation);
 router.get("/worker/info/:id", worker_controller.worker_getByEmailValidation);
 router.get("/worker/data/:id", worker_controller.getUserinfo);
 
+router.get("/worker/data//:localidad/:ocupacion", worker_controller.getWorker_Localities_Ocupation);
+
 //------OBTENER LA PUBLICACION POR LOCALIDAD Y OCUPACION------//
 
 router.get("/worker//:id/:id2", worker_controller.worker_localities_occupations);
