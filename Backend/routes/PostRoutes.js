184b0c5278f5fe403c5d4c6b449a5c6ea53aa204
FernandoDGU <66134044+FernandoDGU@users.noreply.github.com>
2022-05-20 02:35:22 -0500

    Tokens validación en Post

--- Backend/routes/PostRoutes.js
+++ Backend/routes/PostRoutes.js
@@ -4,10 +4,10 @@ const auth = require('../middleware/auth')
 
 const post_controller = require("../controllers/PostController");
 
-router.get("/post", post_controller.post_getall);
-router.post("/post", post_controller.post_create);
-router.get("/post/:id", post_controller.post_getById);
-router.put("/post/:id", post_controller.post_update);
+router.get("/post", auth, post_controller.post_getall);
+router.post("/post", auth, post_controller.post_create);
+router.get("/post/:id", auth, post_controller.post_getById);
+router.put("/post/:id", auth, post_controller.post_update);
 router.delete("/post/:id", post_controller.post_delete);
 
 router.get("/post//worker/:id", auth, post_controller.posts_worker);
--- ourker-app/src/services/PostService.js
+++ ourker-app/src/services/PostService.js
@@ -8,7 +8,13 @@ export const GetPost = async (inputID) =>
     try
     {
         // Comillas invertidas Alt+96
-        const response = await axios.get(`/post/${inputID}`);
+        const user = getUserLogged();
+
+        const response = await axios.get(`/post/${inputID}`, {
+            headers:{
+                'x-access-token': user?.token || ''
+            }
+        });
         return response.data;
     }
     catch(err)
@@ -22,7 +28,13 @@ export const GetAll = async (inputID) =>
     try
     {
         // Comillas invertidas Alt+96
-        const response = await axios.get(`/post//${inputID}`);
+        const user = getUserLogged();
+
+        const response = await axios.get(`/post//${inputID}`, {
+            headers: {
+                'x-access-token': user?.token || ''
+            }
+        });
         return response.data;
     }
     catch(err)
@@ -57,7 +69,13 @@ export const InsertPost = async(inputPost) =>
         const res = await GetCategory(inputPost._category);
         const {_id} = res.data;
         inputPost._category = _id;
-        const response = await axios.post("/post", inputPost);
+
+        const user = getUserLogged();
+        const response = await axios.post("/post", inputPost ,{
+            headers: {
+                'x-access-token': user?.token || ''
+            }
+        });
         
         if(response.status == 200)
         {
@@ -78,7 +96,14 @@ export const UpdatePost = async(inputPost) =>
 {
     try
     {
-        const response = await axios.put(`/post//${inputPost.id}`, inputPost);
+        const user = getUserLogged();
+
+        const response = await axios.put(`/post//${inputPost.id}`, inputPost, {
+            headers: {
+                'x-access-token': user?.token || ''
+            }
+        });
+        
         if(response.status == 200)
         {
             return response.data;
