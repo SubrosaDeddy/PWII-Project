97dbbd6370dc4606512fe0d06a7b2d7acbd43d82
Daniel Luna <danlf9919@gmail.com>
2022-05-20 02:56:03 -0500

    Corregido el error del map en PostsComponent

--- ourker-app/src/components/PostsComponent.js
+++ ourker-app/src/components/PostsComponent.js
@@ -46,45 +46,48 @@ export default function PostsComponent(props) {
     <ThemeProvider theme={theme}>
     <Grid item xs={12} sx={{marginTop: "15px"}}>
         <Paper sx={{ bgcolor: "#F5F5F5", height: "45vh", overflow: "auto" }}>
-          <List>
-            {posts.map((data, index)=>{
-                return (
-                  <Box
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  minHeight="10vh"
-                >
-                  <Button key={index} sx={{ justifyContent: "center", width:"100%"}} onClick={(e) => navigate(`/ViewPost/${data._id}`)}>
-                    <Card sx={{ display: "flex", width: "70%", justifyContent: "space-between", mx:"auto"}}>
-                      <Box sx={{ display: "flex", flexDirection: "column", width: "50%" }}>
-                        <CardContent sx={{ flex: "1 0 auto" }}>
-                          <Typography component="div" variant="h5" fontfamily= "Lexend Deca">
-                            {data.title}
-                          </Typography>
-                          <Typography
-                            variant="subtitle1"
-                            color="text.secondary"
-                            component="div"
-                            fontfamily= "Lexend Deca"
-                          >
-                            {data.description}
-                          </Typography>
-                        </CardContent>
-                      </Box>
-                      <CardMedia
-                        component="img"
-                        sx={{ width: "50%", minWidth: "50%", maxHeight: "140px", minHeight: "140px"}}
-                        image={data.photos[0]}
-                        alt="Post Image"
-                        
-                      />
-                    </Card>
-                  </Button>
-                  </Box>
-                )
-              })}
-          </List>
+          {(posts.length && 
+            <List>
+              {posts.map((data, index)=>{
+                  return (
+                    <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    minHeight="10vh"
+                  >
+                    <Button key={index} sx={{ justifyContent: "center", width:"100%"}} onClick={(e) => navigate(`/ViewPost/${data._id}`)}>
+                      <Card sx={{ display: "flex", width: "70%", justifyContent: "space-between", mx:"auto"}}>
+                        <Box sx={{ display: "flex", flexDirection: "column", width: "50%" }}>
+                          <CardContent sx={{ flex: "1 0 auto" }}>
+                            <Typography component="div" variant="h5" fontfamily= "Lexend Deca">
+                              {data.title}
+                            </Typography>
+                            <Typography
+                              variant="subtitle1"
+                              color="text.secondary"
+                              component="div"
+                              fontfamily= "Lexend Deca"
+                            >
+                              {data.description}
+                            </Typography>
+                          </CardContent>
+                        </Box>
+                        <CardMedia
+                          component="img"
+                          sx={{ width: "50%", minWidth: "50%", maxHeight: "140px", minHeight: "140px"}}
+                          image={data.photos[0]}
+                          alt="Post Image"
+                          
+                        />
+                      </Card>
+                    </Button>
+                    </Box>
+                  )
+                })}
+            </List>
+          )}
+
         </Paper>
     </Grid>
     </ThemeProvider>
